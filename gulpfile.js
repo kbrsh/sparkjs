419f0da81e0e5496adaabcdf35df9cbc4218b7ce
Gopal Shah <simplecooldude1@gmail.com>
2016-11-02 15:59:35 -0700

    gulp and minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,14 @@ var concat = require('gulp-concat');
 var size = require('gulp-size');
 
 
-gulp.task('default', function() {
+gulp.task('build', function() {
+  return gulp.src(['./index.js', './src/*.js'])
+    .pipe(concat('spark.js'))
+    .pipe(size())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', function() {
   return gulp.src(['./index.js', './src/*.js'])
     .pipe(concat('spark.js'))
     .pipe(size())
