bdd796d7a7838a6e63d16521a83fd42360db3cff
Gopal Shah <simplecooldude1@gmail.com>
2016-11-02 15:56:31 -0700

    concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 var gulp = require("gulp");
 var uglify = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
+
+
+gulp.task('default', function() {
+  return gulp.src(['./index.js', './src/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
