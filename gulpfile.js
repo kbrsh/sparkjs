c882f56962ce71c6c9b1fe6e952756a7b09f0105
Gopal Shah <simplecooldude1@gmail.com>
2016-11-02 15:56:56 -0700

    concat to spark

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   return gulp.src(['./index.js', './src/*.js'])
-    .pipe(concat('all.js'))
+    .pipe(concat('spark.js'))
     .pipe(gulp.dest('./dist/'));
 });
