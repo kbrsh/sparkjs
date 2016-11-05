6c9847119630588662dde0804ccfdf2e25a67156
Gopal Shah <simplecooldude1@gmail.com>
2016-11-04 17:45:46 -0700

    bump version

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
 var size = require('gulp-size');
 var header = require("gulp-header");
-var comment = '\/*\r\n* Spark v0.0.0\r\n* Copyright 2016, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
+var comment = '\/*\r\n* Spark v0.0.1\r\n* Copyright 2016, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
 
 
 gulp.task('build', function() {
