0f8480512d5c2d8cda99ef04753c9eeaf2d22497
Gopal Shah <simplecooldude1@gmail.com>
2017-01-07 11:52:43 -0800

    update format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-var Spark = {
-  MarkovChain: MarkovChain,
-  NaiveBayes: NaiveBayes
-}
+(function(root, factory) {
+  (typeof module === "object" && module.exports) ? module.exports = factory() : root.Spark = factory();
+}(this, function() {
+  /* ======= Global Spark ======= */
+  var Spark = {
+    MarkovChain: MarkovChain,
+    NaiveBayes: NaiveBayes
+  }
+
+  //=require MarkovChain.js
+  //=require NaiveBayes.js
+
+  return Spark;
+}));
