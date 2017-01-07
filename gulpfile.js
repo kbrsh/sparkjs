a5abc9ca66d1c84d916d772811cd1c5b64e9879e
Gopal Shah <simplecooldude1@gmail.com>
2017-01-07 11:50:36 -0800

    use include

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var uglify = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
 var size = require('gulp-size');
 var header = require("gulp-header");
+var include = require('gulp-include');
 var comment = '\/*\r\n* Spark v0.1.2\r\n* Copyright 2016, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
 
 
 gulp.task('build', function() {
-  return gulp.src(['./index.js', './src/*.js'])
+  return gulp.src(['./src/index.js'])
+    .pipe($.include())
     .pipe(concat('spark.js'))
     .pipe(header(comment))
     .pipe(size())
