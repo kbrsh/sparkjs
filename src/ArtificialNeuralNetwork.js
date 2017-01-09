e8b514472525f1c29c9912428575a57c4064b179
Gopal Shah <simplecooldude1@gmail.com>
2017-01-08 17:16:27 -0800

    add test

--- src/ArtificialNeuralNetwork.js
+++ src/ArtificialNeuralNetwork.js
@@ -86,3 +86,225 @@ ArtificialNeuralNetwork.prototype.predict = function(inputs) {
   }
   return this.state;
 }
+
+
+function ArtificialNeuralNetwork(opts) {
+	opts = opts || {};
+
+  // Layer Sizes
+	this.inputLayerSize = opts.inputLayerSize || 2;
+  this.outputLayerSize = opts.outputLayerSize || 1;
+  this.hiddenLayerSize = opts.hiddenLayerSize || 3;
+
+  // Learning Rate
+  this.rate = opts.rate || 0.7;
+
+	// Weights for Synapses
+  this.weights = {
+  	inputToHidden: this.randMatrix(this.inputLayerSize, this.hiddenLayerSize),
+    hiddenToOutput: this.randMatrix(this.hiddenLayerSize, this.outputLayerSize)
+  }
+
+  // Neuron States
+  this.neurons = {
+  	hiddenSum: 0,
+    hidden: 0,
+    outputSum: 0,
+    output: 0
+  }
+
+}
+
+ArtificialNeuralNetwork.prototype.randMatrix = function(rows, columns) {
+	var m = [];
+	for(var i = 0; i < rows; i++) {
+  	var arr = [];
+    for(var j = 0; j < columns; j++) {
+    	arr.push(Math.random());
+    }
+    m.push(arr);
+  }
+  return m;
+}
+
+ArtificialNeuralNetwork.prototype.dot = function(matrix1, matrix2) {
+	var result = [];
+  for(var i = 0; i < matrix1.length; i++) {
+  	result[i] = [];
+    for(var j = 0; j < matrix2[0].length; j++) {
+      var total = 0;
+      for(var n = 0; n < matrix1[i].length; n++) {
+        total += matrix1[i][n] * matrix2[n][j]
+      }
+      result[i].push(total)
+    }
+  }
+	return result;
+}
+
+ArtificialNeuralNetwork.prototype.multiply = function(matrix1, matrix2) {
+	var result = [];
+  for(var i = 0; i < matrix1.length; i++) {
+    result[i] = [];
+    for(var j = 0; j < matrix1[i].length; j++) {
+      var total = 0;
+      total += matrix1[i][j] * matrix2[i][j]
+      result[i].push(total)
+    }
+  }
+	return result;
+}
+
+ArtificialNeuralNetwork.prototype.subtract = function(matrix1, matrix2) {
+	var result = [];
+  for(var i = 0; i < matrix1.length; i++) {
+  	result[i] = [];
+    for(var j = 0; j < matrix2[0].length; j++) {
+    	var total = 0;
+      for(var n = 0; n < matrix1[0].length; n++) {
+      	total += matrix1[i][n] - matrix2[i][j];
+      }
+    	result[i].push(total);
+    }
+  }
+	return result;
+}
+
+ArtificialNeuralNetwork.prototype.add = function(matrix1, matrix2) {
+	var result = [];
+  for(var i = 0; i < matrix1.length; i++) {
+  	result[i] = [];
+    for(var j = 0; j < matrix2[0].length; j++) {
+    	var total = 0;
+      for(var n = 0; n < matrix1[0].length; n++) {
+      	total += matrix1[i][n] + matrix2[i][j];
+      }
+    	result[i].push(total);
+    }
+  }
+	return result;
+}
+
+ArtificialNeuralNetwork.prototype.scalar = function(matrix, num) {
+  for(var i = 0; i < matrix.length; i++) {
+    for(var j = 0; j < matrix[i].length; j++) {
+    	matrix[i][j] *= num;
+    }
+  }
+	return matrix;
+}
+
+ArtificialNeuralNetwork.prototype.square = function(matrix) {
+  for(var i = 0; i < matrix.length; i++) {
+    for(var j = 0; j < matrix[i].length; j++) {
+    	matrix[i][j] = Math.pow(matrix[i][j], 2);
+    }
+  }
+	return matrix;
+}
+
+ArtificialNeuralNetwork.prototype.sum = function(matrix) {
+	return matrix.reduce(function(all, row) {
+  	return all + row.reduce(function(total, num) {
+    	return total + num;
+    }, 0);
+  }, 0);
+}
+
+ArtificialNeuralNetwork.prototype.transpose = function(matrix) {
+  return matrix[0].map(function(col, i) {
+  	return matrix.map(function(row) {
+    	return row[i] ;
+  	})
+	});
+}
+
+ArtificialNeuralNetwork.prototype.transform = function(m, fn) {
+  for(var i = 0; i < m.length; i++) {
+    var arr = m[i];
+    m[i] = m[i].map(fn);
+  }
+  return m;
+}
+
+ArtificialNeuralNetwork.prototype.activate = function(arrs) {
+	var activated = [];
+	for(var i = 0; i < arrs.length; i++) {
+  	activated[i] = [];
+    for(var j = 0; j < arrs[i].length; j++) {
+    	activated[i][j] = 1 / (1 + Math.exp(-arrs[i][j]));
+    }
+  }
+  return activated;
+}
+
+ArtificialNeuralNetwork.prototype.deactivate = function(arrs) {
+	var deactivated = [];
+	for(var i = 0; i < arrs.length; i++) {
+  	deactivated[i] = [];
+    for(var j = 0; j < arrs[i].length; j++) {
+    	deactivated[i][j] = Math.exp(-arrs[i][j]) / ((1 + Math.pow(Math.exp(-arrs[i][j]), 2)));
+    }
+  }
+  return deactivated;
+}
+
+ArtificialNeuralNetwork.prototype.train = function(data) {
+	// Move Inputs Through the Net (Forward Propogation)
+  this.data = data;
+  // Matrix of all Neurons in the Hidden Layer
+	this.neurons.hiddenSum = this.dot(this.data.inputs, this.weights.inputToHidden);
+  // Apply Activation
+  this.neurons.hidden = this.activate(this.neurons.hiddenSum);
+  // Matrix of all Nuerons in the Output Layer
+  this.neurons.outputSum = this.dot(this.neurons.hidden, this.weights.hiddenToOutput);
+
+  // Get Prediction after Applying Activation Function
+  this.neurons.output = this.activate(this.neurons.outputSum);
+}
+
+ArtificialNeuralNetwork.prototype.cost = function() {
+	return 0.5 * this.sum((this.square(this.subtract(this.data.inputs, this.neurons.output))));
+}
+
+ArtificialNeuralNetwork.prototype.adjust = function() {
+	// Get Error Amount
+	this.err = this.cost();
+
+  var delta3 = this.multiply(this.scalar(this.subtract(this.data.outputs, this.neurons.output), -1), this.deactivate(this.neurons.outputSum));
+  var dJdW2 = this.dot(this.transpose(this.neurons.output), delta3);
+  var delta2 = this.multiply(this.dot(delta3, this.transpose(this.weights.hiddenToOutput)), this.deactivate(this.neurons.hiddenSum));
+
+}
+
+var NN = new ArtificialNeuralNetwork({
+
+});
+
+NN.train({inputs:[
+	[1, 1],
+  [0, 0]
+], outputs:[
+	[0],
+  [1]
+]});
+
+
+
+
+p.innerHTML = JSON.stringify(NN, null, 2);
+
+setTimeout(function() {
+NN.adjust();
+NN.train({inputs:[
+	[1, 1],
+  [0, 0]
+], outputs:[
+	[0],
+  [1]
+]})
+
+
+
+p.innerHTML = JSON.stringify(NN, null, 2);
+}, 500)
